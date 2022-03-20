18fa1f12d1abaaa41ad942b646f591f3d5af7f1a
mayureshw <mayureshw@githib>
2022-03-20 07:57:05 +0530

    minor corrections to pipe example

--- examples/pntest_pipe.cpp
+++ examples/pntest_pipe.cpp
@@ -3,8 +3,9 @@
 int main()
 {
     auto
-        *excl = new PNDbgPlace("Excl"),
-        *depth = new PNDbgPlace("depth"),
+        *mutex = new PNDbgPlace("mutex"),
+        *free = new PNDbgPlace("free"),
+        *filled = new PNDbgPlace("filled"),
         *push1P = new PNDbgPlace("push1P"),
         *push2P = new PNDbgPlace("push2P"),
         *pop1P = new PNDbgPlace("pop1P"),
@@ -15,29 +16,36 @@ int main()
         *pop1 = new PNDbgTransition("PopReq1"),
         *pop2 = new PNDbgTransition("PopReq2");
     auto
-        *a1 = new PNPTArc(excl,push1),
-        *a2 = new PNPTArc(excl,push2),
-        *a3 = new PNPTArc(excl,pop1),
-        *a4 = new PNPTArc(excl,pop2),
-        *a5 = new PNPTArc(depth,push1),
-        *a6 = new PNPTArc(depth,push2);
+        *a1 = new PNPTArc(mutex,push1),
+        *a2 = new PNPTArc(mutex,push2),
+        *a3 = new PNPTArc(mutex,pop1),
+        *a4 = new PNPTArc(mutex,pop2),
+        *a5 = new PNPTArc(free,push1),
+        *a6 = new PNPTArc(free,push2),
+        *a7 = new PNPTArc(filled,pop1),
+        *a8 = new PNPTArc(filled,pop2),
+        *a9 = new PNPTArc(push1P,push1),
+        *a10 = new PNPTArc(push2P,push2),
+        *a11 = new PNPTArc(pop1P,pop1),
+        *a12 = new PNPTArc(pop2P,pop2);
     auto
-        *a7 = new PNTPArc(push1,excl),
-        *a8 = new PNTPArc(push2,excl),
-        *a9 = new PNTPArc(pop1,excl),
-        *a10 = new PNTPArc(pop2,excl),
-        *a11 = new PNTPArc(pop1,depth),
-        *a12 = new PNTPArc(pop2,depth);
-    auto
-        *a13 = new PNPTArc(push1P,push1),
-        *a14 = new PNPTArc(push2P,push2),
-        *a15 = new PNPTArc(pop1P,pop1),
-        *a16 = new PNPTArc(pop2P,pop2);
+        *a13 = new PNTPArc(push1,mutex),
+        *a14 = new PNTPArc(push2,mutex),
+        *a15 = new PNTPArc(pop1,mutex),
+        *a16 = new PNTPArc(pop2,mutex),
+        *a17 = new PNTPArc(pop1,free),
+        *a18 = new PNTPArc(pop2,free),
+        *a19 = new PNTPArc(push1,filled),
+        *a20 = new PNTPArc(push2,filled);
 
-    PetriNet pn({excl,depth,push1P,push2P,pop1P,pop2P},{push1,push2,pop1,pop2},{a1,a2,a3,a4,a5,a6,a7,a8,a9,a10,a11,a12});
+    PetriNet pn(
+        {mutex,free,filled,push1P,push2P,pop1P,pop2P},
+        {push1,push2,pop1,pop2},
+        {a1,a2,a3,a4,a5,a6,a7,a8,a9,a10,a11,a12,a13,a14,a15,a16,a17,a18,a19,a20}
+        );
     pn.printdot();
-    depth->addtokens(3);
-    excl->addtokens(1);
+    free->addtokens(3);
+    mutex->addtokens(1);
     push1P->addtokens(1);
     push2P->addtokens(1);
     push1P->addtokens(1);
